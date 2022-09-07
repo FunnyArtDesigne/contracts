ee62d3f91580a845bc15870f3259e992d479643a
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-09-07 13:59:29 -0400

    MarketplaceAlt: rewrite Marketplace state in library storage code pattern

--- contracts/marketplace/MarketplaceStorage.sol
+++ contracts/marketplace/MarketplaceStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import { IMarketplace } from "../interfaces/marketplace/IMarketplace.sol";
+import { IMarketplace } from "../interfaces/marketplace/IMarketplaceAlt.sol";
 
 library MarketplaceStorage{
     bytes32 constant MARKETPLACE_STORAGE_POSITION = keccak256("marketplace.storage");
